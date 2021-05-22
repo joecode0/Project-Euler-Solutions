b7620a48b07a093945e9bed2c1b6a82b4ffa175d
joecode0 <joecode0@outlook.com>
2021-05-22 17:07:57 +0100

    Import legacy solutions

--- src/solutions_41-50.py
+++ src/solutions_41-50.py
@@ -7,8 +7,48 @@ logging.basicConfig()
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
+from math import sqrt, floor
+
 def solution_41(args):
-    return args
+    primes_list = get_all_primes(1000000)
+    pandigital_primes = get_all_pandigital_primes(1000000000,primes_list)
+    return max(pandigital_primes)
+
+def test_prime(x, known_primes):
+    if x < 2:
+        return False
+    max_val_to_check = int(sqrt(x))+1
+    current_prime = known_primes[0]
+    i = 0
+    while current_prime < max_val_to_check:
+        if x%current_prime == 0:
+            return False
+        i += 1
+        current_prime = known_primes[i]
+    return True
+
+def get_all_primes(max_val):
+    primes_list = [2,3,5,7,11,13,17]
+    for i in range(18,max_val+1):
+        if i%2 != 0:
+            result = test_prime(i, primes_list)
+            if result:
+                primes_list.append(i)
+    return primes_list
+
+def get_all_pandigital_primes(max_val,primes_list):
+    pandigitals = []
+    for i in range(max_val+1):
+        if i%2 != 0:
+            str_i = str(i)
+            size = len(str_i)
+            if len(set(list(str_i))) == size:
+                if "0" not in str_i and (size+1 < 10) and str(size+1) not in str_i:
+                    result = test_prime(i, primes_list)
+                    if result:
+                        pandigitals.append(i)
+                        #print(i)
+    return pandigitals
 
 def solution_42(args):
     return args
